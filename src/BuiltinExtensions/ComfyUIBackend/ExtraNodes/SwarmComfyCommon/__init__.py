172a2e7a20f4389886d6fb1579142b3b7f9d5ce5
gordo-v1su4 <gordo@DESKTOP-T9VJR2F.localdomain>
2025-08-17 03:14:48 -0400

    Remove ComfyUI random image nodes (cleanup)
    - Removed SwarmRandomImage.py ComfyUI nodes
    - Updated __init__.py to remove references
    - SwarmUI extension is the better solution
    - Keeps codebase clean and focused

--- src/BuiltinExtensions/ComfyUIBackend/ExtraNodes/SwarmComfyCommon/__init__.py
+++ src/BuiltinExtensions/ComfyUIBackend/ExtraNodes/SwarmComfyCommon/__init__.py
@@ -1,6 +1,6 @@
 import os, folder_paths
 
-from . import SwarmBlending, SwarmClipSeg, SwarmImages, SwarmInternalUtil, SwarmKSampler, SwarmLoadImageB64, SwarmLoraLoader, SwarmMasks, SwarmSaveImageWS, SwarmTiling, SwarmExtractLora, SwarmUnsampler, SwarmLatents, SwarmInputNodes, SwarmTextHandling, SwarmReference, SwarmMath, SwarmRandomImage
+from . import SwarmBlending, SwarmClipSeg, SwarmImages, SwarmInternalUtil, SwarmKSampler, SwarmLoadImageB64, SwarmLoraLoader, SwarmMasks, SwarmSaveImageWS, SwarmTiling, SwarmExtractLora, SwarmUnsampler, SwarmLatents, SwarmInputNodes, SwarmTextHandling, SwarmReference, SwarmMath
 
 WEB_DIRECTORY = "./web"
 
@@ -22,7 +22,6 @@ NODE_CLASS_MAPPINGS = (
     | SwarmTextHandling.NODE_CLASS_MAPPINGS
     | SwarmReference.NODE_CLASS_MAPPINGS
     | SwarmMath.NODE_CLASS_MAPPINGS
-    | SwarmRandomImage.NODE_CLASS_MAPPINGS
 )
 
 # TODO: Why is there no comfy core register method? 0.o
